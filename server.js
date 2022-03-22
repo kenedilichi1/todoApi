b7dca540060b6fd8f1d693f0554ff6f8d81447f9
The-Chimsom <okorogladys52@gmail.com>
2022-03-22 13:49:34 -0700

    added validation for completed task

--- server.js
+++ server.js
@@ -11,25 +11,50 @@ const todos = [
 	'Do a major product',
 	'Review code',
 ]
+completed = [];
 
-
+app.use(express.json());
 
 app.get('/todos', function(request, response){
 	response.json(todos);
 });
 
 
-app.delete('/todos/id', (request, response) => {
+app.delete('/todo/id', (request, response) => {
 	response.json({message: 'you deleted from your todos'});
 });
 
-app.put('/completed', function(request, response)  {
-	response.json('task completed')
+app.put('/todo/complete', function(request, response)  {
+
+	let taskId = request.query.taskID;
+
+	taskId = Number(taskId)
+
+	if(isNaN(taskId - 1) || (taskId === 0) || (taskId  > todos.length)) {
+		const result = {
+			error: true,
+			description: `please type in a valid number. A number between 1 and ${todos.length}`
+	
+		} 
+		response.send(result);
+		return;
+	}	
+
+	let removedTodo = todos.splice(taskId - 1, 1)
+
+	completed.push(removedTodo[0])
+
+	const result = {
+		"completed-task": completed,
+		"incompleted-task": todos
+	}
+
+	 response.json(result);
 });
 
-app.post('/todos', function(request, response){
+app.post('/todo', function(request, response){
 	response.json(todos)
-})
+});
 
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
